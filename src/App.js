0df068847283b6fff74030ed83a4f6a194de1554
armanyfelix <armanyfelix@outlook.com>
2021-06-26 15:05:25 -0700

    otro dia otro dolar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Fragment } from 'react';
 import  Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <Fragment>
-      <div className="App">
           <Header 
-          titulo= "Cotizador de prestamos"
+            titulo= "Cotizador de prestamos"
           />
-      </div>
+          <div className="container">
+            <Formulario/>
+          </div>
+
     </Fragment>
       
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,15 @@
-import React, { Fragment } from 'react';
+ import React/*, { Fragment }*/ from 'react';
 
 
-function Header({ titulo }) {
-    return (
-        <Fragment>
-            <h1>{titulo}</h1>
-        </Fragment>
-    )
-}
+// function Header({ titulo }) {
+//     return (
+//         <Fragment>
+//             <h1>{titulo}</h1>
+//         </Fragment>
+//     )
+// }
 
-const Header({ titulo }) => {
-    return (
-        <Fragment>
-            <h1>{titulo}</h1>
-        </Fragment>
-    );
-}
+const Header =({ titulo }) => ( <h1>{titulo}</h1> );
+
+
 export default Header;
