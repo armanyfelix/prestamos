7918bd39fd6e0f6b34240297216ab091117479f0
armanyfelix <armanyfelix@outlook.com>
2021-06-28 21:12:07 -0700

    helpers

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+import { calcularTotal } from '../helpers';
 
-const Formulario = () => {
+const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
+
+    const [error, guardarError] = useState(false);
+
+    // Cuando el usuario hace submit
+    const calcularPrestamo = e => {
+        e.preventDefault();
+
+        // Validar
+        if(cantidad === 0 || plazo === '' ) {
+            guardarError(true);
+            return;
+        }
+
+        // Eliminar el error previo
+        guardarError(false);
+
+        // Realizar la cotización
+        calcularTotal(cantidad, plazo);
+
+    }
+
     return (
-        <form>
-            <div className="row">
-                <div>
-                    <label>Cantidad Prestamo</label>
-                    <input
-                        className="u-full-width"
-                        type="number"
-                        placeholder="Ejemplo: 3000"
-                    />
+        <Fragment>
+            <form onSubmit={ calcularPrestamo }>   
+                <div className="row">
+                    <div>
+                        <label>Cantidad Prestamo</label>
+                        <input
+                            className="u-full-width"
+                            type="number"
+                            placeholder="Ejemplo: 3000"
+                            onChange={ e => guardarCantidad( parseInt(e.target.value)) }    
+                        />
+                    </div>
+                    <div>
+                        <label>Plazo para Pagar</label>
+                        <select
+                            className="u-full-width"
+                            onChange={ e => guardarPlazo( parseInt(e.target.value)) }
+                        >
+                            <option value="">Seleccionar</option>
+                            <option value="3">3 meses</option>
+                            <option value="6">6 meses</option>
+                            <option value="12">12 meses</option>
+                            <option value="24">24 meses</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            value="Calcular"
+                            className="button-primary u-full-width"
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Plazo para Pagar</label>
-                    <select
-                        className="u-full-width"
-                    >
-                        <option value="">Seleccionar</option>
-                        <option value="3">3 meses</option>
-                        <option value="6">6 meses</option>
-                        <option value="12">12 meses</option>
-                        <option value="24">24 meses</option>
-                    </select>
-                </div>
-                <div>
-                    <input
-                        type="submit"
-                        value="Calcular"
-                        className="button-primary u-full-width"
-                    />
-                </div>
-            </div>
-        </form>
+            </form>
+
+            { (error) ? <p className="error"> Todos los campos son obligatorios </p> : null}
+            
+        </Fragment>
+        
     );
 }
 
